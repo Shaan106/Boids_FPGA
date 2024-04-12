0e85527ef91b3801bcc186cd8e920b984ff92892
Shaan Yadav <ay140@duke.edu>
2024-04-12 13:53:11 -0400

    multiple boids?

--- VGA_files/VGAController.v
+++ VGA_files/VGAController.v
@@ -147,11 +147,34 @@ module VGAController(
 
 //		boid_x_vals[1] <= boid_x_vals[1] + 2'b01;
 //		boid_y_vals[1] <= boid_y_vals[1] + 2'b10;
+		integer j;
+
+		initial begin
+			for (j = 1; j < 16; j = j + 1) begin
+				boid_x_vals[j] = boid_x_vals[j] + j;
+				boid_y_vals[j] = boid_y_vals[j] + 1;
+			end
+		end
+
 	end
 
 	
 	always @(posedge clk) begin
-	   isBoidInPixel = ((x == boid_x_vals[0]) && (y == boid_y_vals[0])) ? 1'b1 : 1'b0;
+	//    isBoidInPixel = ((x == boid_x_vals[0]) && (y == boid_y_vals[0])) ? 1'b1 : 1'b0;
+
+		integer i;
+
+		initial begin
+
+			for (i = 0; i < 16; i = i + 1) begin
+				if ((x == boid_x_vals[i]) && (y == boid_y_vals[i])) begin
+					isBoidInPixel = 1'b1;
+				end
+				if ((x != boid_x_vals[i]) || (y != boid_y_vals[i])) begin
+					isBoidInPixel = 1'b0;
+				end
+			end
+		end
 	
 //		isBoidInPixel <= 1'b0;
 //		if ((x == boid_x_vals[0]) && (y == boid_y_vals[0]))

6aa6835c5d1393daffbd3c2f2dc299448c11bbba
Shaan Yadav <39740442+Shaan106@users.noreply.github.com>
2024-04-17 16:33:00 -0400

    chckpoint

--- RAM_resettable.v
+++ RAM_resettable.v
@@ -8,17 +8,32 @@ module RAM_resettable #(
     input write_data;
     output read_data;
     
-    reg read_data_reg;
-    assign read_data = read_data_reg;
-
-    // Two RAMs, one being written/read and one being reset
-    reg[DEPTH-1:0] ram1;
-    reg[DEPTH-1:0] ram2;
-
     // Control which RAM we're writing to
     reg current_ram = 0;
+    
+    reg read_data_reg_1;
+    reg read_data_reg_2;
+    assign read_data = current_ram ? read_data_reg_1 : read_data_reg_2;
+    
+    localparam 
+        TEST_DEPTH = 16,
+        TEST_ADDR_WIDTH = 4;
+        
+
+    // Two RAMs, one being written/read and one being reset
+    reg ram1 [0:DEPTH-1];
+    reg ram2 [0:DEPTH-1];
+    
+//    reg ram1 [0:TEST_DEPTH-1];
+//    reg ram2 [0:TEST_DEPTH-1];
+    
+    wire [TEST_ADDR_WIDTH-1:0] TEST_write_addr, TEST_read_addr;
+    assign TEST_write_addr = write_addr[TEST_ADDR_WIDTH-1:0];
+    assign TEST_read_addr = read_addr[TEST_ADDR_WIDTH-1:0];
+
     // Track address we're resetting right now
     reg [ADDR_WIDTH-1:0] reset_addr = 0;
+//    reg [TEST_ADDR_WIDTH-1:0] reset_addr = 0;
 
     // Swap RAMs when reset is high
     always @(posedge clk) begin
@@ -33,20 +48,20 @@ module RAM_resettable #(
             ram2[reset_addr] <= 0; // reset unused RAM
             if (we)
                 ram1[write_addr] <= write_data;
+//                ram1[TEST_write_addr] <= write_data;
         end else begin
             ram1[reset_addr] <= 0; // reset unused RAM
             if (we)
                 ram2[write_addr] <= write_data;
+//                ram2[TEST_write_addr] <= write_data;
         end
         reset_addr <= reset_addr + 1;
     end
 
     // Read
     always @(posedge clk) begin
-        if (current_ram)
-            read_data_reg <= ram1[read_addr];
-        else
-            read_data_reg <= ram2[read_addr];
+        read_data_reg_1 <= ram1[read_addr];
+        read_data_reg_2 <= ram2[read_addr];
     end
 
 endmodule

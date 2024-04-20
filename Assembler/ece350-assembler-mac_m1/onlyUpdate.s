fc9ef63c45df1e16e8985df40c1f2084bec7b014
Shaan Yadav <ay140@duke.edu>
2024-04-20 15:30:41 -0400

    things

--- Assembler/ece350-assembler-mac_m1/onlyUpdate.s
+++ Assembler/ece350-assembler-mac_m1/onlyUpdate.s
@@ -10,19 +10,19 @@ nop
 # $r26, $k0 = CPU_y_loc
 # $r27, $k1 = which boid/write enable.
 
-addi    $r27, $zero, -1 # we = off
+addi    $r27, $r0, -1 # we = off
 loop:
-    addi    $t0, $t0, 1 # x = x + 1 
-    addi    $t1, $t1, 1 # y = y + 1
+    addi    $r1, $r1, 1 # x = x + 1 
+    addi    $r2, $r2, 1 # y = y + 1
 
-    add     $r28, $t0, $zero # copy x for read to BPU
-    add     $r26, $t1, $zero # copy y for read to BPU
+    add     $r28, $r1, $r0 # copy x for read to BPU
+    add     $r26, $r2, $r0 # copy y for read to BPU
 
-    addi    $r27, $zero, 1 # we = boid 1
+    addi    $r27, $r0, 1 # we = boid 1
 
     nop # time to read
     nop
 
-    addi    $r27, $zero, -1 # we = off
+    addi    $r27, $r0, -1 # we = off
 
     j   loop

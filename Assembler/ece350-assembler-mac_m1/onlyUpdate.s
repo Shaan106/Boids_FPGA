3e863a4780a4ef44a925f224a1a79a4e40c37955
Shaan Yadav <ay140@duke.edu>
2024-04-20 17:12:27 -0400

    fix for constants

--- Assembler/ece350-assembler-mac_m1/onlyUpdate.s
+++ Assembler/ece350-assembler-mac_m1/onlyUpdate.s
@@ -13,9 +13,20 @@ addi    $r27, $0, -1 # we = off
 addi    $t2, $0, 1048576 # x2 = x2 + 1 because of where x loc is taken from
 addi    $t3, $t3, 1 # y2 = y2 + 1
 
+addi    $t4, $0, 1048576
+addi    $t5, $0, 1048576
+
 loop:
-    addi    $t0, $t0, 1 # x1 = x1 + 1 
-    addi    $t1, $t1, 1 # y1 = y1 + 1
+
+    add     $r28, $t4, $0 # copy x3 for read to BPU
+    add     $r26, $t5, $0 # copy y3 for read to BPU
+
+    addi    $r27, $0, 3 # we = boid 3
+
+    nop # time to read
+    nop
+
+    addi    $r27, $0, -1 # we = off
 
     addi    $t2, $t2, 1 # x2 = x2 + 1 
     addi    $t3, $t3, 1 # y2 = y2 + 1
@@ -31,7 +42,8 @@ loop:
     addi    $r27, $0, -1 # we = off
 
     nop
-    nop
+    addi    $t0, $t0, 1 # x1 = x1 + 1 
+    addi    $t1, $t1, 1 # y1 = y1 + 1
     nop
 
     add     $r28, $t0, $0 # copy x1 for read to BPU
